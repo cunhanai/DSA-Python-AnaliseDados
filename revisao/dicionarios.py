249e4093f261ced2be0aea8aa309f8eca7bbe795
cunhanai <dacunhanajulia@gmail.com>
2023-01-05 17:33:20 -0300

    Revisão de Python: dicionarios

--- revisao/dicionarios.py
+++ revisao/dicionarios.py
@@ -13,3 +13,75 @@
 # Com os hash Maps do Java
 
 # Consiste de uma chave e em seguida um valor associado
+
+# COMPARANDO LISTAS E DICIONARIOS
+
+# Lista
+estudantes_list = ["Matteo", 24, "Fernanda", 22, "Tamires", 26, "Cristiano", 25]
+print(estudantes_list)
+
+# Dicionario
+estudantes_dict = {"Matteo":24, "Fernanda":22, "Tamires":26, "Cristiano":25}  # os nomes das pessoas sao as chaves do mapeamento
+print(estudantes_dict)
+
+# As chaves do dicionario podem funcionar como indices
+print(estudantes_dict["Matteo"])
+
+# Adicionar novo mapeamento
+estudantes_dict["Pedro"] = 23
+print(estudantes_dict)
+
+# Redefinir idade
+estudantes_dict["Pedro"] = 34
+print(estudantes_dict["Pedro"])
+
+# Limpar dicionario
+estudantes_dict.clear()
+print(estudantes_dict)
+
+# Deletar dicionario
+del estudantes_dict
+
+# FUNCOES
+
+# Recriando dicionario deletado
+estudantes = {"Matteo":24, "Fernanda":22, "Tamires":26, "Cristiano":25}
+
+print(len(estudantes))
+
+# Extrair valores, chaves e itens
+print(estudantes.keys())
+print(estudantes.values())
+print(estudantes.items())
+
+# CONCATENAR DICIONARIOS
+
+# Novo dicionario
+estudantes2 = {'Erika':28, 'Maria':27, 'Milton':26}
+
+# Atualizar dicionario anterior
+estudantes.update(estudantes2)
+print(estudantes)
+
+# DICIONARIO DE LISTAS
+
+# Criando um dicionario de listas
+dict = {"key1":1230, "key2":[22,356,664,233,1], "key3":['leite', 'maca', 'banana']}
+print(dict)
+
+# Acessando as listas
+print(dict["key1"])
+print(dict['key3'][0].upper())
+
+# Realizar operacoes
+var = dict['key2'][0] - 2
+print(var)
+
+# DICIONARIOS ALINHADOS
+
+# Criando dicionario alinhado
+dict_alinhado = {'key1':{'key2_alinhada':{'key3_alinhada':'Dict alinhado em Python'}}}
+print(dict_alinhado)
+
+# Acessando valor alinhado
+print(dict_alinhado['key1']['key2_alinhada']['key3_alinhada'])
